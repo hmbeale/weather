208dacdc8d0728b06c545ebec3d814120b13d644
Holden Beale <itdbealh@aikencounty.net>
2018-01-29 16:51:10 -0500

    commit tho

--- weather.js
+++ weather.js
@@ -1,12 +1,5 @@
-
-const img = document.querySelector('img')
-fetch('https://api.giphy.com/v1/gifs/random?api_key=q9tkcuxMHVaN0Tw4OS102p3WpPzBohwN&tag=&rating=G', {mode: 'cors'})
+fetch('https:api.openweathermap.org/data/2.5/forecast?id=4569067&APPID=6efa3875a6e7444c141415582068a400',
+{mode: 'cors'})
   .then(function(response) {
-    return response.json()
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url
-  })
-  .catch(e => {
-    console.log(e)
+    console.log(response)
   })
