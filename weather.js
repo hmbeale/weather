454ff2beadce1860fda71175168caf53214d0e34
Holden Beale <itdbealh@aikencounty.net>
2018-02-02 16:53:21 -0500

    hbeale commit

--- weather.js
+++ weather.js
@@ -1,5 +1,36 @@
-fetch('https:api.openweathermap.org/data/2.5/forecast?id=4569067&APPID=6efa3875a6e7444c141415582068a400',
-{mode: 'cors'})
-  .then(function(response) {
-    console.log(response)
-  })
+
+const form = document.querySelector("form");
+
+const fetchWeather = (city) =>{
+  city = city;
+  fetch(`https:api.openweathermap.org/data/2.5/weather?q=${city}
+    &appid=6efa3875a6e7444c141415582068a400`,
+  {mode: 'cors'})
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(response){
+      console.log (response);
+      weather = response.name;
+      displayWeather(response.weather[0].description, response.main.temp);
+    })
+    .catch(function(error){
+      console.log("uh oh error");
+      document.getElementById("display").textContent =
+      `That doesn't seem to be a city.`
+    })
+}
+
+
+const displayWeather = (cityWeather, cityTemp) =>{
+  document.getElementById("display").textContent =
+  `${Math.round(cityTemp - 273.15)}° C with ${cityWeather}.`
+
+}
+
+
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  fetchWeather(form.elements["city"].value);
+})
